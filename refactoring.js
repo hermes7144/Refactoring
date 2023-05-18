6aadc5bfc5eed616ce8e76fbb71ff9cf56ed03f1
hermes7144 <kichancho@gmail.com>
2023-05-18 13:54:14 +0900

    feat:bird polymorphism implement

--- refactoring.js
+++ refactoring.js
@@ -1,35 +1,83 @@
 function plumages(birds) {
-  return new Map(birds.map(b => [b.name, plumage(b)]));
+  return new Map(birds.map(b => createBird(b))
+    .map(bird => [bird.name, bird.plumage]));
 }
 
 function speeds(birds) {
-  return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
+  return new Map(birds.map(b => createBird(b)).map(bird => [bird.name, bird.airSpeedVelocity]));
 }
 
 function plumage(bird) {
-  switch (bird.type)
-  {
-    case '유럽 제비':
-      return '보통이다'
-    case '아프리카 제비':
-      return (bird.numberOfCoconuts > 2) ? '지쳤다' : '보통이다';
-    case '노르웨이 파랑 앵무':
-      return (bird.voltage > 100) ? '그을렸다' : '예쁘다'
-    default:
-      return '알 수 없다'
-  }
+  return createBird(bird).plumage;
 }
 
 function airSpeedVelocity(bird) {
+  return createBird(bird).airSpeedVelocity;
+}
+
+function createBird(bird) {
   switch (bird.type)
   {
     case '유럽 제비':
-      return 35;
+      return new EuropeanSwallow(bird);
     case '아프리카 제비':
-      return 40 - 2 * bird.numberOfCoconuts;
+      return new AfricanSwallow(bird);
     case '노르웨이 파랑 앵무':
-      return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
+      return new NorwegianBlueParrot(bird);
     default:
-      return null;
+      return new Bird(bird);
   }
 }
+
+
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+  get plumage() {
+    return '알 수 없다'
+  }
+
+  get airSpeedVelocity() {
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return '보통이다'
+  }
+  get airSpeedVelocity() {
+    return 35
+  }
+}
+
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return (this.numberOfCoconuts > 2) ? '지쳤다' : '보통이다';
+  }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return (this.voltage > 100) ? '그을렸다' : '예쁘다';
+  }
+  get airSpeedVelocity() {
+    return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+  }
+}
+
+const europeanSwallow = { type: '유럽 제비', name: '유럽 제비' };
+const africanSwallow = { type: '아프리카 제비', numberOfCoconuts: 3, name: '아프리카 제비' };
+const norwegianBlueParrot = { type: '노르웨이 파랑 앵무', voltage: 103, isNailed: true, name: '노르웨이 파랑 앵무' };
+
+
+const birds = [europeanSwallow, africanSwallow, norwegianBlueParrot];
+const birdPlumages = plumages(birds);
+console.log(birdPlumages);
+
+const birdSpeeds = speeds(birds);
+console.log(birdSpeeds);
